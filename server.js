c12a3a200c3b631b5fbabce540d1d0722423d72a
Arielle <arielle.elak@gmail.com>
2022-10-29 14:32:45 -0600

    Residual typo from copy lead to error, resolved, and now button direction and URL /notes pulls current html content

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) =>
 );
 
 // GET Route for notes page
-app.get('/feedback', (req, res) =>
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
