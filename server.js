20c2ec1c1a81d6db02d398b4c8ed79e1405d2347
Arielle <arielle.elak@gmail.com>
2022-10-28 23:03:52 -0600

    Initial server.js page with HTML routes

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET HTML Route for the notes page
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Wildcard HTML route to direct users to the index home page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/index.html'))
+);
+
+// Listen for local port input on 3001
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
