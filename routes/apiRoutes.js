6345507afaf665ec1c7f699aa763be0f58cfc2e6
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-10-29 18:50:17 -0600

    C:/Program Files/Git/api/notes GET and POST worked to write to the JSON file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const apiRouter = require('express').Router();
-const { readAndAppend, readFromFile } = require('../utils/fs');
+const { readAndAppend, readFromFile, writeToFile } = require('../utils/fs');
 
 // GET /api/notes` should read the `db.json` file and return all saved notes as JSON
 apiRouter.get('/', (req, res) => {
@@ -10,11 +10,26 @@ apiRouter.get('/', (req, res) => {
 // POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 apiRouter.post('/', (req, res) => {
     console.info(`${req.method} Request: Post a new note`);
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text
+        };
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully`);
+    } else {
+        res.error('Error in adding note');
+    }
 });
 
 //DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete.
 apiRouter.delete('/', (req, res) => {
     console.info(`${req.method} Request: Delete a note`);
+
 });
 
 // Export these routes to be used by index
